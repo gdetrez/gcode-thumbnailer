8f4869dcba0145f2a7555d51d78817e8e7d492ae
Grégoire Détrez <gregoire@fripost.org>
2022-05-08 12:54:05 +0200

    Import existing code

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+};
+use structopt::StructOpt;
+use systemd::journal;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    input: String,
+    output: String,
+}
+
+fn main() {
+    journal::print(1, &format!("ARGS: {:?}", std::env::args()));
+    let opt = Opt::from_args();
+    let input = opt.input.trim_start_matches("file://");
+    let output = opt.output.trim_start_matches("file://");
+    journal::print(1, &format!("Generating thumbnail for {:?}", input));
+    let file = fs::File::open(&input).unwrap();
+    let bufreader = BufReader::new(file);
+    let mut inthumbnail = false;
+    let mut thumbnail: String = String::new();
+    for line in bufreader.lines() {
+        let line = line.unwrap();
+        match line.strip_prefix("; ") {
+            Some(l) if l.starts_with("thumbnail begin") => {
+                inthumbnail = true;
+                thumbnail = String::new()
+            }
+            Some(l) if l.starts_with("thumbnail end") => inthumbnail = false,
+            Some(l) if inthumbnail => thumbnail.push_str(l),
+            _ => {}
+        }
+    }
+    if !thumbnail.is_empty() {
+        fs::write(output, &base64::decode(&thumbnail).unwrap()).unwrap();
+    }
+}
